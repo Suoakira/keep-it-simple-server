dd2b43a3dd1820c5ffc615c43f7f6a7991b7562b
github username <suoarik@gmail.com>
2018-12-18 10:30:51 +0000

    fixed error with validations

--- db/migrate/20181213102006_create_saving_targets.rb
+++ db/migrate/20181213102006_create_saving_targets.rb
@@ -2,7 +2,7 @@ class CreateSavingTargets < ActiveRecord::Migration[5.2]
   def change
     create_table :saving_targets do |t|
       t.string :name
-      t.datetime :start_data
+      t.datetime :start_date
       t.datetime :end_date
       t.string :target_image
       t.string :plan
